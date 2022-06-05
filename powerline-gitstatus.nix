d20a66cace73d6c82fb300c34fa35ad181eb0a16
Dimitrije Radojević <me@dimitrije.website>
2022-06-05 12:21:41 +0200

    buildInputs => propagatedBuildInputs.

--- powerline-gitstatus.nix
+++ powerline-gitstatus.nix
@@ -3,7 +3,7 @@
 pkgs.python3Packages.buildPythonPackage rec {
   pname = "powerline-gitstatus";
   version = "1.3.1";
-  buildInputs = [ pkgs.powerline ];
+  propagatedBuildInputs = [ pkgs.powerline ];
   src = pkgs.python3Packages.fetchPypi {
     inherit pname version;
     sha256 = "0k5m85vi8p7dzq5c0p60h5dnjm96wllxxp3zvvsfvh88h2jvnlim";
