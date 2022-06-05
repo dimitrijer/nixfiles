90e3a678fbeb099652b51381e87991631b237e0e
Dimitrije Radojević <me@dimitrije.website>
2022-06-05 12:36:35 +0200

    Add meta for powerline-gitstatus.

--- powerline-gitstatus.nix
+++ powerline-gitstatus.nix
@@ -1,11 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.python3Packages.buildPythonPackage rec {
+with pkgs.python3Packages;
+buildPythonPackage rec {
   pname = "powerline-gitstatus";
   version = "1.3.1";
+
   propagatedBuildInputs = [ pkgs.powerline ];
-  src = pkgs.python3Packages.fetchPypi {
+
+  src = fetchPypi {
     inherit pname version;
     sha256 = "0k5m85vi8p7dzq5c0p60h5dnjm96wllxxp3zvvsfvh88h2jvnlim";
+  };
+
+  meta = with pkgs.lib; {
+    homepage = "https://github.com/jaspernbrouwer/powerline-gitstatus";
+    description = "A Powerline segment for showing the status of a Git working copy.";
+    license = licenses.mit;
   };
 }
