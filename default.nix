53d1a8f85715f9d93440301a71d8269a0c8bc1ae
Dimitrije Radojević <me@dimitrije.website>
2024-12-27 10:19:10 +0000

    Use default.nix where possible.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 {}:
 
 { 
-  neovim = import ./neovim/neovim.nix; 
-  python3 = import ./python3.nix;
-  tmux = import ./tmux/tmux.nix;
+  neovim = import ./neovim; 
+  python3 = import ./python3;
+  tmux = import ./tmux;
 }
