ffc3666612ced374d54c426e6a170be7d09e842c
Dimitrije Radojević <me@dimitrije.website>
2026-08-01 10:33:05 +0100

    Fix powerline for python 3.14.

--- python3/default.nix
+++ python3/default.nix
@@ -1,6 +1,30 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
-let 
-  powerline-gitstatus = import ./powerline-gitstatus.nix { pkgs = pkgs; };
-in 
-  pkgs.python3.withPackages (ps: [ ps.powerline powerline-gitstatus ])
+let
+  overlay = final: prev: {
+    python3 = prev.python3.override {
+      packageOverrides = pyFinal: pyPrev: {
+        powerline = pyPrev.powerline.overridePythonAttrs (old: {
+          version = "2.8.4-unstable-2026-03-11";
+          src = final.fetchFromGitHub {
+            owner = "powerline";
+            repo = "powerline";
+            # Fix for python 3.14
+            rev = "11808cbe5c16e4621edcaefa916b5add81eab799";
+            hash = "sha256-L7Xe2FQNXOmMZ94oyqTtpmT50brftP59g2c4l9oyBeI=";
+          };
+        });
+      };
+    };
+
+    # python3Packages is not derived from the python3 attribute, so it has to be
+    # pointed at the overridden interpreter explicitly. Without this,
+    # powerline-gitstatus below would build against the stock package set.
+    python3Packages = final.python3.pkgs;
+  };
+
+  patchedPkgs = pkgs.extend overlay;
+
+  powerline-gitstatus = import ./powerline-gitstatus.nix { pkgs = patchedPkgs; };
+in
+patchedPkgs.python3.withPackages (ps: [ ps.powerline powerline-gitstatus ])
